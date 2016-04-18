9c19f9aecb38f741e8692c608390768d861a144b
fillips92 <filsmu@wp.pl>
2016-04-18 18:13:28 +0200

    Item CRUD

---
+++ ERP/Controllers/ItemsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP.Models;
+
+namespace ERP.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /Items/
+        public ActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        //
+        // GET: /Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //
+        // GET: /Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Items/Create
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        //
+        // GET: /Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //
+        // POST: /Items/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id,Name,QuantityInStock")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        //
+        // GET: /Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        //
+        // POST: /Items/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
